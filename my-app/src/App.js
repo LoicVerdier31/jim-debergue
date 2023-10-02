c2b5bbe6d31a967d01281dfc92ac57a2c4d6afb7
Loïc Verdier <verdiel@hotmail.fr>
2023-10-02 23:03:38 +0200

    ajout index dans le app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import "./index"
 import axios from "axios";
 import { ArrayDetails as LastArraysDetails } from "./components/Galery";
 import GaleryModal from "./components/Galerymodal";
